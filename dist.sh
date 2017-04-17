ca0fa3f290269fef2bd6aefe19950f808ff015c2
Riccardo Metere <rick@metere.it>
2017-04-17 12:51:39 -0700

    Distribute to PyPI.

--- dist.sh
+++ dist.sh
@@ -46,7 +46,7 @@ title "Distribute package"
 
 echo -e "(using config file: \`~/.pypirc\`)"
 
-PYPI_REPOSITORIES=("pipy" "test");
+PYPI_REPOSITORIES=("test" "pipy");
 NUM_PYPI_REPOSITORIES=${#PYPI_REPOSITORIES[@]}
 if [ "$NUM_PYPI_REPOSITORIES" -gt 1 ]; then
     for ITEM in ${PYPI_REPOSITORIES[@]}; do
